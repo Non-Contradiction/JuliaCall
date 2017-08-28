abbef145765da1c43a3a363911cab16eb6e241ed
Non-Contradiction <lch34677@gmail.com>
2017-08-28 14:03:17 -0400

    Initial stuff.

---
+++ R/aaa.R
@@ -0,0 +1,19 @@
+.julia <- new.env(parent = emptyenv())
+
+julia <- new.env(parent = .julia)
+
+#' Check whether julia is available on the path.
+#'
+#' \code{julia_check} checks if julia is available on the path.
+#'
+#' @return whether julia is available on the path.
+#'
+#' @examples
+#' julia_check()
+#'
+#' @export
+julia_check <- function(){
+    tryCatch(system('julia -e "println(1)"', intern = TRUE) == "1",
+             warning = function(war){},
+             error = function(err) FALSE)
+}
