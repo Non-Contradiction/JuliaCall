29818ae850f7e86f369b825fc7812d6c97512e0b
Non-Contradiction <lch34677@gmail.com>
2017-08-30 14:07:29 -0400

    Rename package help topic to julia_package.

--- R/package.R
+++ R/package.R
@@ -5,20 +5,20 @@
 #' @return julia_installed_package will return the version number of the julia package,
 #'     "nothing" if the package is not installed.
 #'
-#' @name package
+#' @name julia_package
 NULL
 
-#' @rdname package
+#' @rdname julia_package
 #' @export
 julia_install_package <- julia$install_package <-
     function(pkg_name) julia$call("Pkg.add", pkg_name, need_return = FALSE)
 
-#' @rdname package
+#' @rdname julia_package
 #' @export
 julia_installed_package <- julia$installed_package <-
     function(pkg_name) julia$call("JuliaCall.installed_package", pkg_name)
 
-#' @rdname package
+#' @rdname julia_package
 #' @export
 julia_install_package_if_needed <- julia$install_package_if_needed <-
     function(pkg_name){
@@ -27,12 +27,12 @@ julia_install_package_if_needed <- julia$install_package_if_needed <-
     }
 }
 
-#' @rdname package
+#' @rdname julia_package
 #' @export
 julia_update_package <- julia$update_package <-
     function(...) julia$do.call("Pkg.update", list(...))
 
-#' @rdname package
+#' @rdname julia_package
 #' @export
 julia_library <- julia$library <- function(pkg_name){
     tryCatch(julia$command(paste0("using ", pkg_name)),
