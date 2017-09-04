e3695175dc7538de2617fe9c5e365e38591c84fc
Non-Contradiction <lch34677@gmail.com>
2017-09-04 16:22:30 -0400

    Add tests for julia_assign.

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -17,4 +17,7 @@ test_that("test of the basic functionality", {
     expect_equal({julia_command("a = sqrt(2)"); julia_eval_string("a")}, sqrt(2))
     expect_true(julia_exists("sqrt"))
     expect_output(julia_help("sqrt"))
+
+    expect_equal({julia_assign("x", sqrt(2)); julia_eval_string("x")}, sqrt(2))
+    expect_equal({julia_assign("rsqrt", sqrt); julia_eval_string("rsqrt(2)")}, sqrt(2))
 })
