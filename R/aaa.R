7033267b389f142324185b7aa85646b005dd405d
Non-Contradiction <lch34677@gmail.com>
2017-09-05 14:14:59 -0400

    julia_locate to locate the position of julia, julia_line to pass command line to julia, and julia_check is defunct.

--- R/aaa.R
+++ R/aaa.R
@@ -5,18 +5,35 @@ julia <- new.env(parent = .julia)
 
 #' Check whether julia is available on the path.
 #'
-#' \code{julia_check} checks if julia is available on the path.
-#'
-#' @return whether julia is available on the path.
-#'
-#' @examples
-#' julia_check()
+#' \code{julia_check} checks if julia is available on the path. It is defunct.
 #'
 #' @export
 julia_check <- function(){
+    warning("julia_check is defunct, the functionality of julia_check is combined into julia_setup.")
     tryCatch(system('julia -e "println(1)"', intern = TRUE) == "1",
              warning = function(war){},
              error = function(err) FALSE)
+}
+
+julia_locate <- function(JULIA_HOME = NULL){
+    if (is.null(JULIA_HOME)) {
+        JULIA_HOME <- getOption("JULIA_HOME")
+    }
+
+    if (is.null(JULIA_HOME)) {
+        tryCatch(system("julia -E \"println(JULIA_HOME)\"", intern = TRUE)[1],
+                 warning = function(war){},
+                 error = function(err) NULL)
+    }
+    else {
+        tryCatch(system(paste0(file.path(JULIA_HOME, "julia"), " -E \"println(JULIA_HOME)\""), intern = TRUE)[1],
+                 warning = function(war){},
+                 error = function(err) NULL)
+    }
+}
+
+julia_line <- function(command, ...){
+    system(paste(file.path(.julia$bin_dir, "julia"), command), ...)
 }
 
 newer <- function(x, y) utils::compareVersion(x, y) >= 0
