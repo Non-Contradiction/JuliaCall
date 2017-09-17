d04dbad97b534b0e867089c35a6eeb79277117bf
Non-Contradiction <lch34677@gmail.com>
2017-09-17 11:24:41 -0400

    Save time on CI.

--- tests/testthat/test_julia_package.R
+++ tests/testthat/test_julia_package.R
@@ -4,8 +4,8 @@ test_that("test of functionality related to julia_package", {
     skip_on_cran()
     julia <- julia_setup()
 
-    expect_null(julia_install_package("Optim"))
-    expect_null(julia_install_package_if_needed("Optim"))
-    expect_gte(utils::compareVersion(julia_installed_package("Optim"), "0.7.8"), 0)
-    expect_null(julia_library("Optim"))
+    expect_null(julia_install_package("RCall"))
+    expect_null(julia_install_package_if_needed("RCall"))
+    expect_gte(utils::compareVersion(julia_installed_package("RCall"), "0.7.4"), 0)
+    expect_null(julia_library("RCall"))
 })
