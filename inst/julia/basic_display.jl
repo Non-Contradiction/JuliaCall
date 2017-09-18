a94a5d1ad2ec4021d7372092848844f6e5db82c8
Non-Contradiction <lch34677@gmail.com>
2017-09-17 22:14:03 -0400

    Display manager to manage basic_display

--- inst/julia/basic_display.jl
+++ inst/julia/basic_display.jl
@@ -7,3 +7,18 @@ basic_repl = Base.REPL.BasicREPL(out_terminal)
 basic_display = Base.REPL.REPLDisplay(basic_repl)
 
 Base.pushdisplay(basic_display)
+
+type DisplayManager
+    repl_display :: Base.REPL.REPLDisplay
+    location :: Int64
+end
+
+DisplayManager(repl_display :: Base.REPL.REPLDisplay) = DisplayManager(repl_display, 0)
+
+function proceed(dm :: DisplayManager)
+    print(readstring(seek(dm.repl_display.repl.terminal.out_stream, dm.location)))
+    dm.location = dm.repl_display.repl.terminal.out_stream.ptr-1
+    print("  \n")
+end
+
+basic_display_manager = DisplayManager(basic_display)
