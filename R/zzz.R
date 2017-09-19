0d2039b763dd4e6ff33f7de7fb44c3ecd1eee4d0
Non-Contradiction <lch34677@gmail.com>
2017-09-19 14:03:03 -0400

    Correct one minor mistake.

--- R/zzz.R
+++ R/zzz.R
@@ -171,7 +171,7 @@ julia_setup <- function(JULIA_HOME = NULL, verbose = TRUE, force = FALSE, useRCa
         julia_command("eval(Base, :(is_interactive = true));")
     }
 
-    if (getOption("jupyter.in_kernel")) {
+    if (isTRUE(getOption("jupyter.in_kernel"))) {
         julia_command("Base.pushdisplay(JuliaCall.irjulia_display);")
     }
 
