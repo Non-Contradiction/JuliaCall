749acd37a72bd78cc3e5b8cb832bd26104d17ab8
Non-Contradiction <lch34677@gmail.com>
2017-10-01 18:22:45 -0400

    #9, need to check if `RCall` is precompiled with the current R.

---
+++ inst/julia/RCallprepare.jl
@@ -0,0 +1,20 @@
+## Thanks to randy3k for pointing this out,
+## `RCall` needs to be precompiled with the current R.
+## <https://github.com/Non-Contradiction/JuliaCall/issues/9>
+## as well as coming up with the solution
+
+check_args(ARGS)
+
+Rhome = ARGS[1]
+
+ENV["R_HOME"] = Rhome
+
+if Pkg.installed("RCall") == nothing
+    Pkg.add("RCall")
+end;
+
+using RCall
+
+if RCall.Rhome != Rhome
+    Base.compilecache("RCall")
+end
