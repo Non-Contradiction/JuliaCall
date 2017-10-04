095b81242f5cf111e14df9083621d11867fd7dba
Non-Contradiction <lch34677@gmail.com>
2017-10-04 16:13:55 -0400

    Better implementation for JuliaObject.

--- inst/julia/JuliaObject.jl
+++ inst/julia/JuliaObject.jl
@@ -2,24 +2,35 @@ import RCall.sexp
 
 julia_object_count = 0
 julia_object_dict = Dict()
+julia_object_reverse_dict = Dict()
+
+type JuliaObjectName
+    s :: String
+end
 
 type JuliaObject
-    name :: String
-    JuliaObject(name :: String) = new(name)
+    name :: JuliaObjectName
+    JuliaObject(name :: JuliaObjectName) = new(name)
 end
 
 function sexp(x :: JuliaObject)
-    r = sexp(x.name)
+    r = sexp(x.name.s)
     setclass!(r, sexp("JuliaObject"))
     r
 end
 
 function new_obj(obj)
     global julia_object_count
-    julia_object_count += 1
-    name = String(Symbol(:J_, julia_object_count))
-    julia_object_dict[name] = obj
-    JuliaObject(name)
+    if !haskey(julia_object_reverse_dict, obj)
+        julia_object_count += 1
+        name = String(Symbol(:J_, julia_object_count))
+        julia_object_dict[name] = obj
+        julia_object_reverse_dict[obj] = name
+    else
+        name = julia_object_reverse_dict[obj]
+    end
+
+    JuliaObject(JuliaObjectName(name))
 end
 
 JuliaObject(x :: JuliaObject) = x
