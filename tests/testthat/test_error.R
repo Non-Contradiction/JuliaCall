2ec32ced8d18709d75815df4968c0fb9990241e3
Non-Contradiction <lch34677@gmail.com>
2017-10-05 15:58:09 -0400

    Use julia_eval instead of julia_eval_string in tests.

--- tests/testthat/test_error.R
+++ tests/testthat/test_error.R
@@ -4,7 +4,7 @@ test_that("test of the error handling functionality", {
     skip_on_cran()
     julia <- julia_setup()
 
-    expect_error(julia_eval_string("abc"))
+    expect_error(julia_eval("abc"))
     expect_error(julia_command("abc"))
     expect_error(julia_call("sqrt", -1))
     expect_error(julia_call("sqrt", 1, 2))
