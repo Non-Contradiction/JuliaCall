ea7fc3cb6507808c721cd9eeb5ff4721c8442071
Non-Contradiction <lch34677@gmail.com>
2017-10-13 14:55:35 -0400

    Improve plotsViewer examples, and pyplot backend is not stable.

--- example/plotsViewer.R
+++ example/plotsViewer.R
@@ -2,17 +2,19 @@ library(JuliaCall)
 julia_setup()
 plotsViewer()
 julia_library("Plots")
-julia_command("pyplot()")
-julia_command("Plots.plot([1 2], [3 5])")
 
 julia_command("plotlyjs()")
-julia_command("Plots.plot([1 2], [3 4])")
+julia_command("Plots.plot([1, 2], [3, 4])")
 
 julia_command("plotly()")
-julia_command("Plots.plot([1 2], [3 6])")
+julia_command("Plots.plot([1, 2], [3, 6])")
 
 julia_command("gr()")
-julia_command("Plots.plot([1 2], [3 7])")
+julia_command("Plots.plot([1, 2], [3, 7])")
+
+## pyplot backend is not stable.
+##julia_command("pyplot()")
+##julia_command("Plots.plot([1, 2], [3, 5])")
 
 ## This currently doesn't work because of Julia issue 14577
 ## https://github.com/JuliaLang/julia/issues/14577
