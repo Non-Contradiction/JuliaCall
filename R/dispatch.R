f2a54ca5e5aab4cc0bba3fdfee84b6fddb720412
Non-Contradiction <lch34677@gmail.com>
2017-10-21 11:53:26 -0400

    Temporarily fix the dispatch method. Prepare to make JuliaObject S4 class instead of S3. It’s more strict and less problematic.

--- R/dispatch.R
+++ R/dispatch.R
@@ -17,7 +17,19 @@ length.JuliaObject <- function(x) julia_call("length", x)
 #' @export
 exp.JuliaObject <- function(x) julia_call("exp.", x)
 #' @export
-sum.JuliaObject <- function(x, ...) julia_call("sum", x)
+sum.JuliaObject <- function(x, ..., na.rm = FALSE){
+    stopifnot(length(list(...)) == 0)
+    julia_call("sum", x)
+}
 #' @export
 `[.JuliaObject` <- function(x, i) julia_call("getindex", x, i)
 
+#' @export
+as.character.JuliaObject <- function(x, ...)
+    julia_call("string", x)
+#' @export
+as.list.JuliaObject <- function(x, ...)
+    julia_call("RCall.sexp", julia_eval("RCall.VecSxp"), x)
+#' @export
+as.double.JuliaObject <- function(x, ...)
+    julia_call("RCall.sexp", julia_eval("RCall.RealSxp"), x)
