94c66b2fdc97d729882a5415493f624ca695d17b
Non-Contradiction <lch34677@gmail.com>
2017-10-24 21:00:04 -0400

    JuliaTuple convert to S3 class of JuliaTuple based on R list.

--- inst/julia/setup.jl
+++ inst/julia/setup.jl
@@ -24,6 +24,7 @@ include("JuliaObject.jl")
 ## include("JuliaArray.jl")
 include("asR.jl")
 include("dispatch.jl")
+include("JuliaTuple.jl")
 
 function transfer_list(x)
     rcopy(RObject(Ptr{RCall.VecSxp}(x)))
