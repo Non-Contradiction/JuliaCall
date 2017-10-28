a0156d98aaa594d59654fdc82919f072b39cf25b
Non-Contradiction <lch34677@gmail.com>
2017-10-27 21:01:36 -0400

    Add test for #18.

--- tests/testthat/test_JuliaObject.R
+++ tests/testthat/test_JuliaObject.R
@@ -5,4 +5,17 @@ test_that("test of the JuliaObject", {
     julia <- julia_setup()
 
     expect_equal(julia_eval("1//2") ^ 2, 0.25)
+
+    julia_command("
+    type Mytype
+        num::Int
+        den::Int
+        ans::Float64
+    end")
+    julia_eval("Mytype(1, 12, 1/12)")
+    r <- julia_eval("Mytype(1, 12, 1/12)")
+    expect_length(r, 1)
+    expect_equal(julia_call("getfield", r, quote(den)), 12)
+    expect_equal(julia_call("getfield", r, quote(num)), 1)
+    expect_equal(julia_call("getfield", r, quote(ans)), 1 / 12)
 })
