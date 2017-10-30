e00965be2f6620951b862a61d5df1f7fd3dc4fcd
Non-Contradiction <lch34677@gmail.com>
2017-10-29 20:32:29 -0400

    Do julia_setup() before loading the testthat library because of issue #19.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,8 @@
 Sys.setenv("R_TESTS" = "")
-library(testthat)
 library(JuliaCall)
+## Need to do julia_setup() before loading the testthat library.
+## Because dyn.load libjulia after library(testthat) is problematic.
+julia_setup()
+library(testthat)
 
 test_check("JuliaCall")
