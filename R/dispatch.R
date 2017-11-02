d4f27e0bcc1ea1c958cdcccf9b9430537e9454d7
Non-Contradiction <lch34677@gmail.com>
2017-11-02 16:24:25 -0400

    More S3 methods instead of S4 ones.

--- R/dispatch.R
+++ R/dispatch.R
@@ -37,84 +37,20 @@ as.integer.JuliaObject <- function(x, ...)
 as.logical.JuliaObject <- function(x, ...)
     julia_call("JuliaCall.asLogical", x)
 
-#' S4 Group Generic Functions for JuliaObject.
-#'
-#' S4 Group Generic Functions for JuliaObject, which includes common mathematical
-#'   functions and operators.
-#'
-#' @param e1,e2  objects.
-#'
-#' @name S4JuliaObjectGeneric
-NULL
-
 fdot <- function(x) paste0(as.character(x), ".")
 
-## Compare Group
+## Ops Group
 
-dot_e1_e2 <- function(e1, e2) julia_call(fdot(.Generic), e1, e2)
-
-#' @rdname S4JuliaObjectGeneric
 #' @export
-setMethod("Compare", "JuliaObject", dot_e1_e2)
-#' @rdname S4JuliaObjectGeneric
+Ops.JuliaObject <- function(e1, e2) julia_call(fdot(.Generic), e1, e2)
 #' @export
-setMethod("Compare", c("ANY", "JuliaObject"), dot_e1_e2)
-#' @rdname S4JuliaObjectGeneric
+`%%.JuliaObject` <- function(e1, e2) julia_call("mod.", e1, e2)
 #' @export
-setMethod("Compare", c("JuliaObject", "JuliaObject"), dot_e1_e2)
-
-## Arith Group
-
-#' @rdname S4JuliaObjectGeneric
+`%/%.JuliaObject` <- function(e1, e2) julia_call("div.", e1, e2)
 #' @export
-setMethod("Arith", "JuliaObject", dot_e1_e2)
-#' @rdname S4JuliaObjectGeneric
+`&.JuliaObject` <- function(e1, e2) as.logical(e1) & as.logical(e2)
 #' @export
-setMethod("Arith", c("ANY", "JuliaObject"), dot_e1_e2)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("Arith", c("JuliaObject", "JuliaObject"), dot_e1_e2)
-
-jmod <- function(e1, e2) julia_call("mod.", e1, e2)
-
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("%%", "JuliaObject", jmod)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("%%", c("ANY", "JuliaObject"), jmod)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("%%", c("JuliaObject", "JuliaObject"), jmod)
-
-jdiv <- function(e1, e2) julia_call("div.", e1, e2)
-
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("%/%", "JuliaObject", jdiv)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("%/%", c("ANY", "JuliaObject"), jdiv)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("%/%", c("JuliaObject", "JuliaObject"), jdiv)
-
-## Logic Group
-
-jlogic <- function(e1, e2){
-    methods::callGeneric(as.logical(e1), as.logical(e2))
-}
-
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("Logic", "JuliaObject", jlogic)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("Logic", c("ANY", "JuliaObject"), jlogic)
-#' @rdname S4JuliaObjectGeneric
-#' @export
-setMethod("Logic", c("JuliaObject", "JuliaObject"), jlogic)
-
+`|.JuliaObject` <- function(e1, e2) as.logical(e1) | as.logical(e2)
 
 ## Math Group
 
