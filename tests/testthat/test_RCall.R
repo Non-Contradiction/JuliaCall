01b3af65da4d7153152a70cefced42a5b8c4521f
Non-Contradiction <lch34677@gmail.com>
2017-11-12 09:20:20 -0500

    More tests for error handling in RCall through JuliaCall.

--- tests/testthat/test_RCall.R
+++ tests/testthat/test_RCall.R
@@ -7,4 +7,8 @@ test_that("test of using RCall through JuliaCall", {
     expect_equal(julia_eval('R"1"'), 1)
     expect_equal(julia_eval('reval("1")'), 1)
     expect_equal(julia_eval('rcall(:sqrt, 2)'), sqrt(2))
+
+    expect_error(julia_eval('R"aaa"'))
+    expect_error(julia_eval('reval("aaa")'))
+    expect_error(julia_eval('rcall(:sqrt, "aaa")'))
 })
