aadc297318dc272ee657976f91d017148e748d63
Non-Contradiction <lch34677@gmail.com>
2017-11-15 20:08:27 -0500

    Don't do julia_setup on CRAN, otherwise it will be problematic. For example, 64bit Julia won't work with 32bit R.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,11 @@ Sys.setenv("R_TESTS" = "")
 library(JuliaCall)
 ## Need to do julia_setup() before loading the testthat library.
 ## Because dyn.load libjulia after library(testthat) is problematic.
-julia_setup()
+## And do this only when not on CRAN,
+## otherwise it will be problematic for win32 and things like that.
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+    julia_setup()
+}
 library(testthat)
 
 test_check("JuliaCall")
