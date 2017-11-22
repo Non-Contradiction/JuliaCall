4646524daf40b6fb47d6fdda69771e92327a233f
Randy Lai <randy.cs.lai@gmail.com>
2017-11-21 19:59:00 -0500

    setting R_LD_LIBRARY_PATH should be enough

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,3 @@
-Sys.setenv("R_TESTS" = "")
-library(JuliaCall)
-## Need to do julia_setup() before loading the testthat library.
-## Because dyn.load libjulia after library(testthat) is problematic.
-## And do this only when not on CRAN,
-## otherwise it will be problematic for win32 and things like that.
-if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-    julia_setup()
-}
 library(testthat)
-
+library(JuliaCall)
 test_check("JuliaCall")
