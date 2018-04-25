fb61a61a836891625de7bd5b3148bd1f8b6a1f31
Non-Contradiction <lch34677@gmail.com>
2018-04-25 18:42:24 -0400

    Add documentation for autowrap.

--- R/JuliaObjectExt.R
+++ R/JuliaObjectExt.R
@@ -1,5 +1,15 @@
 .juliaobjtable <- new.env(parent = emptyenv())
 
+#' Use automatic wrapper for julia type.
+#'
+#' \code{autowrap} tells `JuliaCall` to use automatic wrapper for julia type.
+#'
+#' @param type the julia type to wrap.
+#' @param fields names of fields to be included in the wrapper.
+#'   If the value is NULL, then every julia fields will be included in the wrapper.
+#' @param methods names of methods to be overloaded for the wrapper.
+#'
+#' @export
 autowrap <- function(type, fields = NULL, methods = c()){
     addExt(type, fields, methods)
     cmd <- paste0('@eval JuliaCall @suppress_err begin sexp(x :: Main.',
