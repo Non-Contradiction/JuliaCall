d783f279c6dedac81f8d2f5c3be9120524bbece7
Non-Contradiction <lch34677@gmail.com>
2018-04-27 15:21:45 -0400

    Bug fixing. Need to remove "" names for the unamed arguments to have correct behavior for named arguments in `julia_call`.

--- R/argument.R
+++ R/argument.R
@@ -3,5 +3,7 @@ separate_arguments <- function(arglist){
         return(list(unamed = arglist, named = list()))
     }
     idx <- names(arglist) != ""
-    return(list(unamed = arglist[!idx], named = arglist[idx]))
+    unamed <- arglist[!idx]
+    names(unamed) <- NULL
+    return(list(unamed = unamed, named = arglist[idx]))
 }
