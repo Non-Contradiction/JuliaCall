968bd93c71e598d5d9578270c4b25c284f549a88
Non-Contradiction <lch34677@gmail.com>
2018-05-22 10:57:06 -0400

    Implementation of more arguments for `rep.JuliaObject`.

--- R/internal_generic.R
+++ R/internal_generic.R
@@ -8,6 +8,10 @@
 # }
 
 #' @export
-rep.JuliaObject <- function(x, times, ...){
+rep.JuliaObject <- function(x, times = 1, length.out = NA, each = 1){
+    if (!is.na(length.out)) {
+        times <- ceiling(length.out / length(x))
+        return(julia_call("JuliaCall.rep", x, as.integer(times))[1:length.out])
+    }
     julia_call("JuliaCall.rep", x, as.integer(times))
 }
