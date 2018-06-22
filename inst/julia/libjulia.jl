e99ae953181667b42dcaec5b68041059e7e36950
Non-Contradiction <lch34677@gmail.com>
2018-06-21 22:04:39 -0400

    Use ismatch which doesn't require Compat and seems to be more stable in v0.6.

--- inst/julia/libjulia.jl
+++ inst/julia/libjulia.jl
@@ -1,9 +1,7 @@
-using Compat
-
 dlls = Libdl.dllist()
 
 libjulia = (filter(Libdl.dllist()) do dl
-    return occursin(Regex("^libjulia(?:.*)\\.$(Libdl.dlext)(?:\\..+)?\$"), basename(dl))
+    return ismatch(Regex("^libjulia(?:.*)\\.$(Libdl.dlext)(?:\\..+)?\$"), basename(dl))
 end)[1]
 
 print(libjulia)
