da983ade4b8d6f61a6d0da007dfd1040e89d2d06
Non-Contradiction <lch34677@gmail.com>
2018-07-01 16:25:07 -0400

    Factor out the jcall interface function.

--- inst/include/JuliaObject/JuliaObject.hpp
+++ inst/include/JuliaObject/JuliaObject.hpp
@@ -14,8 +14,10 @@ public:
 #include <Rcpp.h>
 using namespace Rcpp;
 
+const Environment julia = Environment("package:JuliaCall");
+const Function jcall = julia["julia_call"];
+
 JuliaObject::JuliaObject(SEXP x1){
-    Environment julia("package:JuliaCall"); Function jcall = julia["julia_call"];
     x = jcall("JuliaCall.JuliaObject", x1);
 };
 
@@ -23,9 +25,9 @@ SEXP wrap(JuliaObject j){
     return SEXP(j.x);
 }
 
-#define UNARY_FUNC(fname, jname) JuliaObject fname(JuliaObject j){Environment julia("package:JuliaCall"); Function jcall = julia["julia_call"]; return jcall(#jname, j);}
+#define UNARY_FUNC(fname, jname) JuliaObject fname(JuliaObject j){return jcall(#jname, j);}
 
-#define BINARY_FUNC(fname, jname) JuliaObject fname(JuliaObject j1, JuliaObject j2){Environment julia("package:JuliaCall"); Function jcall = julia["julia_call"]; return jcall(#jname, j1, j2);}
+#define BINARY_FUNC(fname, jname) JuliaObject fname(JuliaObject j1, JuliaObject j2){return jcall(#jname, j1, j2);}
 
 UNARY_FUNC(log, log.);
 
