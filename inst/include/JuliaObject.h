bef0e8c7087aa83f1cfc9af56cce20519c4d09a7
Non-Contradiction <lch34677@gmail.com>
2018-07-01 15:55:53 -0400

    A simple interface to use JuliaObject in Rcpp code.

---
+++ inst/include/JuliaObject.h
@@ -0,0 +1,9 @@
+#ifndef JuliaObject__JuliaObject__h
+#define JuliaObject__JuliaObject__h
+
+#include "JuliaObject/JuliaObject.hpp"
+// possibly other
+// header files
+// to include
+
+#endif // hinterface__hinterface__h
