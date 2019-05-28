2447a0334a4fad82ed67ad82f90bc64906a4e722
Non-Contradiction <lch34677@gmail.com>
2019-05-28 16:07:15 -0400

    Add test for commit #03fee46. Related to #106.

--- tests/testthat/test_julia_package.R
+++ tests/testthat/test_julia_package.R
@@ -8,4 +8,9 @@ test_that("test of functionality related to julia_package", {
     expect_null(julia_install_package_if_needed("RCall"))
     # expect_gte(utils::compareVersion(julia_installed_package("RCall"), "0.7.4"), 0)
     expect_null(julia_library("RCall"))
+
+    ## test installation of packages from URL
+    if (utils::compareVersion(julia$VERSION, "0.6.5")) {
+        expect_null(julia_install_package("https://github.com/JuliaIO/Suppressor.jl"))
+    }
 })
