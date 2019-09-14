65846a0afef3072456ef7ec97f6b0db139688ab1
lch34677 <lch34677@gmail.com>
2019-09-14 13:16:47 -0400

    Simple exploration about arrow.

---
+++ inst/julia/arrow.jl
@@ -0,0 +1,18 @@
+using Arrow
+import RCall.sexp
+import RCall.rcopy
+import RCall: RClass, rcopytype
+
+## Julia Tuple will be convert to S3 class JuliaTuple based on R list
+
+function sexp(x ::Primitive{Float64})
+    sexp(x[:])
+end
+
+const Ras_vector = R"function(x) x[['as_vector']]()"
+
+function rcopy(::Type{Primitive}, x::Ptr{EnvSxp})
+    rcopy(RCall.rcall_p(Ras_vector, x))
+end
+
+rcopytype(::Type{RClass{Symbol("arrow::Array")}}, x::Ptr{EnvSxp}) = Primitive
