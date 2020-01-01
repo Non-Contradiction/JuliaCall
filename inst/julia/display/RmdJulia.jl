9902ab9f182209d5ab76bc567fa6e4272ecfad78
lch34677 <lch34677@gmail.com>
2020-01-01 13:58:40 -0500

    Refactoring with require.

--- inst/julia/display/RmdJulia.jl
+++ inst/julia/display/RmdJulia.jl
@@ -20,14 +20,39 @@ function text_display(display::RmdDisplay, x)
     text_display_in_R(s)
 end
 
-function display(display::RmdDisplay, x)
-    try
-        if isa(x, Main.Plots.Plot)
+if julia07
+    using Requires
+else
+    macro require_str() end
+end
+
+@require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" @eval begin
+
+    function display(display::RmdDisplay, x::Plots.Plot)
+        try
             plot_display(display, x)
             return
+        catch e;
         end
-    catch e;
+        try
+            text_display(display, x)
+            return
+        catch e;
+        end
+        throw(MethodError)
     end
+
+    function plot_display(display::RmdDisplay, x::Plots.Plot)
+        path = rcopy(begin_plot_in_R())
+        mkpath(dirname(path))
+        Plots.savefig(x, path)
+        finish_plot_in_R()
+    end
+
+end
+
+
+function display(display::RmdDisplay, x)
     try
         text_display(display, x)
         return
