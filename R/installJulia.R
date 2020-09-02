c965a0a290a28c5d1560ceb63768d725272a8e0a
Chris de Graaf <me@cdg.dev>
2020-09-02 12:11:23 -0500

    Look up latest version

--- R/installJulia.R
+++ R/installJulia.R
@@ -1,15 +1,24 @@
-julia_version <- "v1.5.0"
-
 julia_default_install_dir <- function(){
     dir <- if (require("rappdirs")) {
-        file.path(user_data_dir("JuliaCall"), "julia", julia_version)
+        file.path(user_data_dir("JuliaCall"), "julia")
     } else {
         NULL
     }
     return(dir)
 }
 
-julia_tgz_url <- function(){
+julia_latest_version <- function(){
+    url <- "https://raw.githubusercontent.com/JuliaBinaryWrappers/Julia_jll.jl/master/Project.toml"
+    file <- tempfile()
+    download.file(url, file)
+    toml <- readChar(file, 1024)
+    match <- regexec("version = \"(.*)\\+(.*?)\"", toml)
+    captures <- regmatches(toml, match)
+    captures[[1]][2] <- paste("v", captures[[1]][2], sep = "")
+    return(captures[[1]][2:3])
+}
+
+julia_tgz_url <- function(version, build){
     arch <- if (.Machine$sizeof.pointer == 8) {
         "x86_64"
     } else {
@@ -26,27 +35,30 @@ julia_tgz_url <- function(){
         stop("Unknown or unsupported OS")
     }
     url <- sprintf(
-        "https://github.com/JuliaBinaryWrappers/Julia_jll.jl/releases/download/Julia-%s%%2B0/Julia.%s.%s-%s-libgfortran4-cxx11.tar.gz",
-        julia_version, julia_version, arch, os
+        "https://github.com/JuliaBinaryWrappers/Julia_jll.jl/releases/download/Julia-%s+%s/Julia.%s.%s-%s-libgfortran4-cxx11.tar.gz",
+        version, build, version, arch, os
     )
     return(url)
 }
 
-julia_save_install_dir <- function(prefix){
+julia_save_install_dir <- function(dir){
     depot <- Sys.getenv("JULIA_DEPOT_PATH", unset = path.expand("~/.julia"))
     prefs <- file.path(depot, "prefs")
     dir.create(prefs, recursive = TRUE, showWarnings = FALSE)
-    cat(file.path(prefix, "bin"), file = file.path(prefs, "JuliaCall"))
+    cat(file.path(dir, "bin"), file = file.path(prefs, "JuliaCall"))
 }
 
 install_julia <- function(prefix = julia_default_install_dir()){
     if (is.null(prefix)) {
         stop("rappdirs is not installed and prefix was not provided")
     }
-    url <- julia_tgz_url()
+    version_build <- julia_latest_version()
+    version <- version_build[1]
+    build <- version_build[2]
+    url <- julia_tgz_url(version, build)
     file <- tempfile()
     download.file(url, file)
-    untar(file, exdir=prefix)
-    julia_save_install_dir(prefix)
-    Sys.setenv(JULIA_HOME = file.path(prefix, "bin"))
+    dest <- file.path(prefix, version)
+    untar(file, exdir=dest)
+    julia_save_install_dir(dest)
 }
