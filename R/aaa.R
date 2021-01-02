ec493c0d09fb60ab8d9f3edd366356db1295a7f0
lch34677 <lch34677@gmail.com>
2021-01-01 23:30:03 -0500

    Improve the logic of searching julia on Windows.

--- R/aaa.R
+++ R/aaa.R
@@ -39,9 +39,9 @@ julia_locate <- function(JULIA_HOME = NULL){
                 # if the path is not defined, then try to construct it manually
                 if(appdata_local_path == "") {
                     windows_login_id <- Sys.info()[["login"]]
-                    if(windows_login_id == "unknown") {
-                        stop("The Windows login is 'unknown'. Can not find julia executable")
-                    }
+                    # if(windows_login_id == "unknown") {
+                    #     stop("The Windows login is 'unknown'. Can not find julia executable")
+                    # }
                     appdata_local_path <- file.path("C:/Users/", windows_login_id, "AppData/Local")
                 }
 
@@ -52,15 +52,17 @@ julia_locate <- function(JULIA_HOME = NULL){
                 # which of these folers start with "Julia"
                 x = ld[sort(which(substr(ld,1,5) == "Julia"))]
 
-                if(length(x) == 0) {
-                    stop(sprintf("Can not find the Julia installation in the default installation path '%s'", appdata_local_path))
-                }
-                # TODO if interactive() let the user choose a version of Julia
-                # keep the lastest version of Julia as that is likeley to be the default
-                x = x[length(x)]
+                # if(length(x) == 0) {
+                #     stop(sprintf("Can not find the Julia installation in the default installation path '%s'", appdata_local_path))
+                # }
+                if (length(x) > 0) {
+                    # TODO if interactive() let the user choose a version of Julia
+                    # keep the lastest version of Julia as that is likeley to be the default
+                    x = x[length(x)]
 
-                # filter the ld for folders that starts with Julia
-                julia_bin <- file.path(appdata_local_path, x, "bin/julia.exe")
+                    # filter the ld for folders that starts with Julia
+                    julia_bin <- file.path(appdata_local_path, x, "bin/julia.exe")
+                }
             } else {
                 julia_bin <- "julia"
             }
